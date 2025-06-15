b1cb0c247b7260803661f063518dfbc7f45b0ef5
baldazi <baldaazi99@gmail.com>
2025-06-15 23:35:07 +0200

    get module name
    check if go.mod file exist and parse it to get the module name

--- main.go
+++ main.go
@@ -1,16 +1,30 @@
 package main
 
 import (
-	"fmt"
 	"flag"
+	"fmt"
+	"golang.org/x/mod/modfile"
+	"log"
+	"os"
 )
 
-func main(){
-
+func main() {
+	// define all flags
 	from := flag.String("from", "", "current module name")
 	to := flag.String("to", "", "new module name")
-
 	flag.Parse()
+	// parse mod file
+	data, err := os.ReadFile("go.mod")
+	if err != nil {
+		log.Fatal("Cannot find go.mod — make sure you're inside a Go module directory.")
+	}
 
+	mf, err := modfile.Parse("go.mod", data, nil)
+
+	if err != nil {
+		log.Fatal("Failed to parse go.mod: invalid or corrupted format.")
+	}
+
+	fmt.Println("nom du module :", mf.Module.Mod.Path)
 	fmt.Println("hello world!", *from, *to)
 }
